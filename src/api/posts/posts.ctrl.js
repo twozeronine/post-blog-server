44f68621a6615120a3272b1770cb92363ff96822
twozeronine <amdusias209@gmail.com>
2021-03-10 21:13:39 +0900

    username 과 tag로 포스트 필터링하기

--- src/api/posts/posts.ctrl.js
+++ src/api/posts/posts.ctrl.js
@@ -74,7 +74,7 @@ export const write = async (ctx) => {
 };
 
 /* 
-  GET /api/posts/
+  GET /api/posts?username=&tag=&page=
 */
 
 export const list = async (ctx) => {
@@ -86,14 +86,22 @@ export const list = async (ctx) => {
     ctx.status = 400;
     return;
   }
+  const { tag, username } = ctx.query;
+  // tag, username 값이 유효하면 객체 안에 넣고 , 그렇지 않으면 넣지 않음
+  const query = {
+    ...(username ? { 'user.username': username } : {}),
+    ...(tag ? { tags: tag } : {}),
+  };
+
   try {
-    const posts = await Post.find()
+    // 위에서 받아온 query객체로 MongoDB에서 값을 찾음
+    const posts = await Post.find(query)
       .sort({ _id: -1 })
       .limit(10)
       .skip((page - 1) * 10)
       .lean()
       .exec();
-    const postCount = await Post.countDocuments().exec();
+    const postCount = await Post.countDocuments(query).exec();
     ctx.set('Last-Page', Math.ceil(postCount / 10));
     ctx.body = posts.map((post) => ({
       ...post,
