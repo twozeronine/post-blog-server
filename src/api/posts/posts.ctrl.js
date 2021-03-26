b103265fd711a7b44c69fce6727202d30b5808ea
twozeronine <amdusias209@gmail.com>
2021-03-26 11:33:45 +0900

    sanitize-html 라이브러리 사용 악성 HTML 삽입을 걸러주게끔 구현함.

--- src/api/posts/posts.ctrl.js
+++ src/api/posts/posts.ctrl.js
@@ -1,8 +1,33 @@
 import Post from '../../models/post';
 import mongoose from 'mongoose';
 import Joi from 'joi';
+import sanitizeHtml from 'sanitize-html';
 
 const { ObjectId } = mongoose.Types;
+
+const sanitizeOption = {
+  allowedTags: [
+    'h1',
+    'h2',
+    'b',
+    'i',
+    'u',
+    's',
+    'p',
+    'ul',
+    'ol',
+    'li',
+    'blockquote',
+    'a',
+    'img',
+  ],
+  allowedAttributes: {
+    a: ['href', 'name', 'target'],
+    img: ['src'],
+    li: ['class'],
+  },
+  allowedSchems: ['data', 'http'],
+};
 
 // 미들웨어
 
@@ -61,7 +86,7 @@ export const write = async (ctx) => {
   const { title, body, tags } = ctx.request.body;
   const post = new Post({
     title,
-    body,
+    body: sanitizeHtml(body, sanitizeOption),
     tags,
     user: ctx.state.user,
   });
@@ -76,6 +101,14 @@ export const write = async (ctx) => {
 /* 
   GET /api/posts?username=&tag=&page=
 */
+
+// html을 없애고 내용이 너무 길면 200자로 제한하는 함수
+const removeHtmlAndShorten = (body) => {
+  const filtered = sanitizeHtml(body, {
+    allowedTags: [],
+  });
+  return filtered.length < 200 ? filtered : `${filtered.slice(0, 200)}...`;
+};
 
 export const list = async (ctx) => {
   // query는 문자열이기 때문에 숫자로 변환해 주어야 합니다
@@ -105,8 +138,7 @@ export const list = async (ctx) => {
     ctx.set('Last-Page', Math.ceil(postCount / 10));
     ctx.body = posts.map((post) => ({
       ...post,
-      body:
-        post.body.length < 200 ? post.body : `${post.body.slice(0, 200)}...`,
+      body: removeHtmlAndShorten(post.body),
     }));
   } catch (e) {
     ctx.throw(500, e);
@@ -160,9 +192,13 @@ export const update = async (ctx) => {
     ctx.body = result.error;
     return;
   }
-
+  const nextData = { ...ctx.request.body }; // 객체를 복사하고
+  //body 값이 주어졌으면 HTML 필터링
+  if (nextData.body) {
+    nextData.body = sanitizeHtml(nextData.body, sanitizeOption);
+  }
   try {
-    const post = await Post.findByIdAndUpdate(id, ctx.request.body, {
+    const post = await Post.findByIdAndUpdate(id, nextData, {
       new: true, //이 값을 설정하면 업데이트된 데이터를 반환합니다.
       // false일 때는 업데이트되기 전의 데이터를 반환합니다.
     }).exec();
